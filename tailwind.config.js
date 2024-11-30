1df6a0b4824d2c8e008f389ebf600f6ece0cb982
garrethil <garrethildebrandt@gmail.com>
2024-11-29 23:25:41 -0500

    setup colors in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#060000",
+          text: "#ffce46",
+        },
+        secondary: {
+          DEFAULT: "#ffffff",
+          text: "#e32f8d",
+        },
+      },
+    },
   },
   plugins: [],
 };
