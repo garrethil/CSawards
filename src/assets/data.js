783db76245d8e84ab11b271f2e9856790bee53f5
garrethil <garrethildebrandt@gmail.com>
2024-11-29 22:56:37 -0500

    setup data.js with award data

---
+++ src/assets/data.js
@@ -0,0 +1,70 @@
+const awards = [
+  {
+    title: "ELSA AWARD",
+    name: "Sara Audrain",
+    desc: "Best icy frozen stare combined with laser focus and consistent calm amidst chaos",
+    pic: "",
+  },
+  {
+    title: "SWEAT EQUITY AWARD",
+    name: "Haley Kriksic",
+    desc: "Sportiest employee also doing a reno award while balancing 100 accounts",
+    pic: "",
+  },
+  {
+    title: "THIS IS FINE AWARD",
+    name: "Allison Madigan",
+    desc: "Light me on fire and I still won't stress out",
+    pic: "",
+  },
+  {
+    title: "ROMANCING THE STONE AWARD",
+    name: "Sean E",
+    desc: "Agency king of romancing the client (IMC)",
+    pic: "",
+  },
+  {
+    title: "TRIPLE THREAT AWARD",
+    name: "Abbie Jack",
+    desc: "Most likely to become famous (for running, for her hair, for her acting, for her modelling)",
+    pic: "",
+  },
+  {
+    title: "LIFE OF THE PARTY AWARD",
+    name: "Max Morandin",
+    desc: "Number one C+S party animal who's always in a good mood",
+    pic: "",
+  },
+  {
+    title: "DOOGIE HOWSER AWARD",
+    name: "Ophelie Zalcmanis-Lai",
+    desc: "Biggest brain, youngest age, coolest presenter",
+    pic: "",
+  },
+  {
+    title: "WEST COAST WONDER AWARD",
+    name: "Andrew Parsons",
+    desc: "Most organized & proficient: writer, expense submitter, social copy master creative ever",
+    pic: "",
+  },
+  {
+    title: "OPTIMIST PRIME AWARD",
+    name: "Jess Beattie",
+    desc: "Always smiling, always positive, always helpful",
+    pic: "",
+  },
+  {
+    title: "BRILLIANCE RESILIENCE AWARD",
+    name: "Saf",
+    desc: "Comes back after mat leave, jumps on %$$%$$# Humber and still shows up with a fantastic attitude",
+    pic: "",
+  },
+  {
+    title: "NEW KID ON THE BLOCK AWARD",
+    name: "Jon Lane",
+    desc: "Maybe a bit early to know a ton about you, but we think your new kid energy is exactly what we need",
+    pic: "",
+  },
+];
+
+export default awards;
