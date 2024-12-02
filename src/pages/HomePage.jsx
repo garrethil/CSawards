e65745b34d55dd770f67a4cb00faeb3b5cb9de12
garrethil <garrethildebrandt@gmail.com>
2024-12-02 18:06:06 -0500

    add bgimage class back to home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 function HomePage() {
   return (
-    <div className="h-screen w-screen bg-cover bg-center bg-no-repeat flex items-center justify-center">
+    <div className="h-screen w-screen bg-homepage bg-cover bg-center bg-no-repeat flex items-center justify-center">
       <div className="absolute top-0 left-0 w-full h-[350px] bg-gradient-to-b from-black to-transparent"></div>
       {/* Tint Overlay */}
       <div className="absolute inset-0 bg-black bg-opacity-60"></div>
