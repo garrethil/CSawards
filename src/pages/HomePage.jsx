9c31903af6898a150cbaaaa253a5581a50a301b1
garrethil <garrethildebrandt@gmail.com>
2024-12-02 20:00:18 -0500

    correct header button functionality for mobile views

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,30 +1,37 @@
 function HomePage() {
   const handleScrollToSection = (e, sectionId) => {
-    e.preventDefault(); // Prevent the default anchor link behavior
+    e.preventDefault();
     const section = document.getElementById(sectionId);
     if (section) {
-      section.scrollIntoView({ behavior: "smooth" }); // Scroll to the section smoothly
+      section.scrollIntoView({ behavior: "smooth" });
+    } else {
+      console.warn(`Section with ID '${sectionId}' not found.`);
     }
   };
+
   return (
     <div className="h-screen w-screen bg-homepageSmall md:bg-homepageBig bg-cover bg-center bg-no-repeat flex items-center justify-center">
+      {/* Gradient Overlay */}
       <div className="absolute top-0 left-0 w-full h-[350px] bg-gradient-to-b from-black to-transparent"></div>
+
       {/* Tint Overlay */}
       <div className="absolute inset-0 bg-black bg-opacity-60"></div>
 
       {/* Header */}
-      <button
-        className="absolute text-secondary hover:text-primary-text hover:underline flex top-0 w-full bg-primary p-8 shadow-lg"
-        onClick={(e) => handleScrollToSection(e, "awards")}
-      >
-        <h2 className="text-2xl md:text-4xl p-4 sm:p-6 md:p-7 ml-auto font-bebas tracking-wider">
-          2024 winners
-        </h2>
-      </button>
+      <div className="absolute bg-primary top-0 left-0 w-full flex justify-end items-center p-4 sm:p-6 z-50">
+        <button
+          className="text-secondary hover:text-primary-text hover:underline p-3 sm:p-4 md:p-6 rounded-md shadow-lg"
+          onClick={(e) => handleScrollToSection(e, "awards")}
+        >
+          <h2 className="text-lg sm:text-xl md:text-4xl font-bebas tracking-wider">
+            2024 Winners
+          </h2>
+        </button>
+      </div>
 
       {/* Centered Logo and Text */}
       <div className="relative flex flex-col items-center justify-center h-full">
-        {/* Container with consistent width */}
+        {/* Content Container */}
         <div className="flex flex-col justify-center items-center text-center">
           <h1 className="text-secondary-text font-bold text-[40px] md:text-[48px] lg:text-[60px] my-2 sm:mb-3 md:mb-4 font-bebas tracking-wider">
             1st Annual
