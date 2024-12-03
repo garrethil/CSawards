cf908362729b61ff5738240b5417948d7a4d44b0
garrethil <garrethildebrandt@gmail.com>
2024-12-03 12:02:34 -0500

    add gradient back to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,6 +11,8 @@ function HomePage() {
 
   return (
     <div className="relative h-screen w-screen bg-homepageSmall md:bg-homepageBig bg-cover bg-center bg-no-repeat flex items-center justify-center">
+      {/* Gradient Overlay */}
+      <div className="absolute top-0 left-0 w-full h-[350px] bg-gradient-to-b from-black to-transparent"></div>
       {/* Tint Overlay */}
       <div className="absolute inset-0 bg-cover bg-black bg-opacity-60"></div>
 
