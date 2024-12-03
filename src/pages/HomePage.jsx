cb0743f39c3a10d8141bbbd066e61a22cbea7727
garrethil <garrethildebrandt@gmail.com>
2024-12-03 10:59:57 -0500

    add cover property to tint of bgimage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -12,7 +12,7 @@ function HomePage() {
   return (
     <div className="h-screen w-screen bg-homepageSmall md:bg-homepageBig bg-cover bg-center bg-no-repeat flex items-center justify-center">
       {/* Tint Overlay */}
-      <div className="absolute inset-0 bg-black bg-opacity-60"></div>
+      <div className="absolute inset-0 bg-cover bg-black bg-opacity-60"></div>
 
       {/* Header */}
       <div className="absolute bg-primary top-0 left-0 w-full flex justify-end items-center p-4 sm:p-6 z-50">
